0cc3fccb38a7f1fae90cfe6bca11d99694c7e1b0
Sven Haider <sven.haider@web.de>
2020-05-13 00:19:49 +0200

    create landingpage design

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2020_05_12_095449) do
+ActiveRecord::Schema.define(version: 2020_05_12_111903) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
