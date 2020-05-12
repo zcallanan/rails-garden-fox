39b8c712a2e13ba9d35d7769115fcd4fb7b5ce08
Zach Callanan <caveofmonsters@gmail.com>
2020-05-12 16:53:12 +0200

    Garden index skeleton

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# user = User.new(
+#   email: Faker, created_at: "2020-05-11 12:38:32", updated_at: "2020-05-12 11:20:40", address: "1 Arnulfstraße", country_code: "DE", phone_number: "+49 12123131313", birth_date: "1980-01-01", admin: true
+
+#   )
+
+
+Garden.destroy_all
+
+garden = Garden.new(
+  name: "Awesome Garden",
+  description: "A beautiful garden",
+  location: "Munich",
+  price: 500,
+  house_rules: "No parties or pets. No fun!",
+  filter_categories: "BBQ, Fireplace",
+  size: 250,
+  capacity: 25,
+  garden_type: "Garden",
+  availability: true,
+  user_id: 2
+  )
+garden.save
+
+garden = Garden.new(
+  name: "An okay garden",
+  description: "A modest garden",
+  location: "Munich",
+  price: 300,
+  house_rules: "Keep noise down.",
+  filter_categories: "TV, Fireplace, WiFi",
+  size: 100,
+  capacity: 10,
+  garden_type: "Rooftop",
+  availability: true,
+  user_id: 1
+  )
+garden.save
