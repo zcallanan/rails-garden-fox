c29916bdffb8114a1cfac4d3ec92d06a70e53ba5
Benjamin Steidl <localartpool@gmail.com>
2020-05-12 14:52:42 +0200

    Add Users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+Garden.destroy_all
+
+15.times do
+  User.create(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    address: Faker::Address.street_address,
+    country_select: Faker::Address.country,
+    email: Faker::Internet.email,
+    phone_number: Faker::Business.credit_card_number,
+    birth_date: Faker::Date.birthday,
+    )
+end
