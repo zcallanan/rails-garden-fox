0dc4039af4f1c7c692f1c97aaab3cb5d42296aa6
Sven Haider <sven.haider@web.de>
2020-05-14 19:01:54 +0200

    styled serchbar

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_14_094427) do
+ActiveRecord::Schema.define(version: 2020_05_14_164906) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,6 +67,7 @@ ActiveRecord::Schema.define(version: 2020_05_14_094427) do
     t.float "longitude"
     t.integer "duration"
     t.integer "start_time"
+    t.boolean "featured", default: false
     t.index ["user_id"], name: "index_gardens_on_user_id"
   end
 
